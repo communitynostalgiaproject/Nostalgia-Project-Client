b56c87dd3e00242b7c211d4e508147de2935645c
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2024-01-12 10:31:43 -0500

    getById Reaction request successful

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -6,13 +6,8 @@ const LandingPage: React.FC = () => {
 
     useEffect(() => {
         const createReaction = async() => {
-            let deleted = await ReactionRequest.post({
-                reaction: "meToo",
-                userId: "6541795c8c2ca0edcda512df",
-                experienceId: "659c2be7198da1cbcf8d41d6",
-                createdDate: "2023-01-08"
-            })
-            return deleted
+            let reaction = await ReactionRequest.getById("659c3809fcb2d898d87818d7")
+            return reaction
         };
 
         createReaction().then(res => console.log(res));
