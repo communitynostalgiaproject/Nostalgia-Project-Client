8e81df4688ce8878ffe401e3efa9d5fefa97ed59
David White <david@david-white.dev>
2024-01-16 13:18:28 -0700

    Added fetch data method to experiencesRequest class

--- src/api/experiences.request.ts
+++ src/api/experiences.request.ts
@@ -1,11 +1,19 @@
 import { CRUDRequestBase } from "./CRUDRequestBase";
+import { getApiBase } from "./helpers";
+import axios from "axios";
 
 export class ExperienceRequest extends CRUDRequestBase {
-
     constructor(model:string) {
         super(model);
     };
 
+    fetchData = async () => {
+      let response = await axios.get(`${getApiBase()}/users/fetchData`, {
+          withCredentials: true
+      });
+
+      return response.data;
+    }
 };
 
 export default new ExperienceRequest("experiences");
