baf7da3d89902076d294d6c5717797f6305daab7
JordanJamesTaylor <jordjamestaylor@gmail.com>
2024-02-06 17:34:55 -0500

    Event card redesign

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './shared/components/navbar';
 import LandingPage from "./pages/landingPage";
 // import SciencePage from "./components/science/SciencePage";
-// import EventsPage from "./components/events/EventsPage";
+import EventsPage from "./pages/events/EventsPage";
 // import AboutPage from "./components/about/AboutPage";
 
 import "./App.css";
@@ -16,7 +16,7 @@ const App: React.FC = () => {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 {/* <Route path="/research" element={<SciencePage />} /> */}
-                {/* <Route path="/events" element={<EventsPage />} /> */}
+                <Route path="/events" element={<EventsPage />} />
                 {/* <Route path="/community_outreach" element={<EventsPage />} /> */}
                 {/* <Route path="/our_team" element={<EventsPage />} /> */}
                 {/* <Route path="/about_us" element={<AboutPage />} /> */}
