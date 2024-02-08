6ff125019896067d004b2868de17f6a634cfba39
David White <david@david-white.dev>
2024-02-08 10:21:16 -0700

    Configured UserForm story file rendering function to grab 'currentUser' with useQuery and pass it to the component. Created tests for expected form behavior

--- src/components/forms/UserForm/index.tsx
+++ src/components/forms/UserForm/index.tsx
@@ -1,13 +1,143 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Container,
+  Typography,
+  Button,
+  TextField,
+  Box
+} from '@mui/material';
 
 interface UserFormProps {
-  user: any;
-};
+  user: {
+    _id: string;
+    displayName: string;
+  };
+}
 
 const UserForm: React.FC<UserFormProps> = ({ user }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedDisplayName, setEditedDisplayName] = useState(user.displayName);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setEditedDisplayName(user.displayName); // Reset editedDisplayName to initial value
+  };
+
+  const handleSave = () => {
+    console.log("Saving new displayName: ", editedDisplayName);
+    setIsEditing(false);
+  };
+
+  const handleDelete = () => {
+
+  };
+
   return (
-    <>
-    </>
+    <Container
+      sx={{
+        width: "100%",
+        minWidth: "300px",
+        border: "1px solid black"
+      }}
+      data-testid="UserForm-FormContainer"
+    >
+      <Box
+        sx={{
+          padding: "25px 0px"
+        }}
+      >
+        <Typography
+          variant="h5"
+        >
+          User Settings
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          gap: "10px"
+        }}
+      >
+        <Box
+          sx={{
+
+          }}
+        >
+          <Typography variant="h6">
+            Display Name:
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center"
+            }}
+          >
+            {!isEditing ? (
+              <>
+                <Typography
+                  data-testid="UserForm-DisplayNameText"
+                >
+                  {user.displayName}
+                </Typography>
+                <Button
+                  onClick={handleEdit}
+                  data-testid="UserForm-EditDisplayNameButton"
+                >
+                  Edit
+                </Button>
+              </>
+            ) : (
+              <TextField
+                variant="outlined"
+                value={editedDisplayName}
+                onChange={(e) => setEditedDisplayName(e.target.value)}
+                fullWidth
+                data-testid="UserForm-UpdateDisplayNameField"
+              />
+            )}
+          </Box>
+        </Box>
+        <Button
+          onClick={handleDelete}
+          color="secondary"
+          data-testid="UserForm-DeleteAccountButton"
+        >
+          Delete Account
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          marginTop: 2,
+          display: "flex",
+          justifyContent: "flex-end"
+        }}
+      >
+        {isEditing && (
+          <>
+            <Button
+              onClick={handleCancel}
+              style={{ marginRight: "8px" }}
+              data-testid="UserForm-CancelEditButton"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={handleSave}
+              color="primary"
+              data-testid="UserForm-SaveEditsButton"
+            >
+              Save
+            </Button>
+          </>
+        )}
+      </Box>
+    </Container>
   );
 };
 
---
+++ src/stories/forms/userForm.stories.tsx
@@ -0,0 +1,157 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { createUserFetchHandler } from '../util/mswHandlers';
+import { useQuery, QueryClient, QueryClientProvider } from 'react-query';
+import { StoryFn } from '@storybook/react';
+import UserForm from '../../components/forms/UserForm';
+import axios from 'axios';
+import { Box } from '@mui/material';
+import React from 'react';
+
+const mockUser = {
+  "_id": "111111111",
+  "googleId": "12121212",
+  "email": "faker@test.com",
+  "displayName": "Test User"
+};
+
+const QueryContextWrapper: StoryFn= ({ children }) => {
+  const client = new QueryClient();
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+const UserFormWrapper: React.FC = ({ ...args }) => {
+  const { data: user } = useQuery("currentUser", async () => {
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/users/fetchData`, { withCredentials: true });
+    return res.data;
+  });
+
+  // Render UserForm with the fetched user data
+  return (
+    <>
+      {user && <UserForm user={user} />}
+    </>
+  );
+};
+
+const meta = {
+  title: 'Forms/User Form',
+  component: UserForm,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  render: () => <QueryContextWrapper><UserFormWrapper /></QueryContextWrapper>
+} satisfies Meta<typeof UserForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const Form: Story = {
+  args: {
+    user: {_id: "9032390", displayName: "Something went wrong..."},
+  },
+  parameters: {
+    msw: {
+      handlers: {
+        fetchUser: [createUserFetchHandler(200, mockUser)]
+      }
+    }
+  },
+};
+
+export const CancelEditTest: Story = {
+  args: {
+    user: {_id: "9032390", displayName: "Something went wrong..."},
+  },
+  parameters: {
+    msw: {
+      handlers: {
+        fetchUser: [createUserFetchHandler(200, mockUser)]
+      }
+    }
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await waitFor(() => {
+      expect(canvas.getByTestId("UserForm-DisplayNameText")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DisplayNameText").textContent).toBe(mockUser.displayName);
+      expect(canvas.getByTestId("UserForm-EditDisplayNameButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DeleteAccountButton")).toBeInTheDocument();
+    });
+
+    await userEvent.click(canvas.getByTestId("UserForm-EditDisplayNameButton"));
+    await waitFor(() => {
+      expect(canvas.queryByTestId("UserForm-DisplayNameText")).not.toBeInTheDocument();
+      expect(canvas.queryByTestId("UserForm-EditDisplayNameButton")).not.toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-UpdateDisplayNameField")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DeleteAccountButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-CancelEditButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-SaveEditsButton")).toBeInTheDocument();
+    });
+
+    await userEvent.type(canvas.getByTestId("UserForm-UpdateDisplayNameField"), "Updated Name");
+    await userEvent.click(canvas.getByTestId("UserForm-CancelEditButton"));
+    await waitFor(() => {
+      expect(canvas.queryByTestId("UserForm-UpdateDisplayNameField")).not.toBeInTheDocument();
+      expect(canvas.queryByTestId("UserForm-CancelEditButton")).not.toBeInTheDocument();
+      expect(canvas.queryByTestId("UserForm-SaveEditsButton")).not.toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DisplayNameText")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-EditDisplayNameButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DeleteAccountButton")).toBeInTheDocument();
+    });
+  }
+};
+
+export const SaveEditsTest: Story = {
+  args: {
+    user: {_id: "9032390", displayName: "Something went wrong..."},
+  },
+  parameters: {
+    msw: {
+      handlers: {
+        fetchUser: [createUserFetchHandler(200, mockUser)]
+      }
+    }
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await waitFor(() => {
+      expect(canvas.getByTestId("UserForm-DisplayNameText")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DisplayNameText").textContent).toBe(mockUser.displayName);
+      expect(canvas.getByTestId("UserForm-EditDisplayNameButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DeleteAccountButton")).toBeInTheDocument();
+    });
+
+    await userEvent.click(canvas.getByTestId("UserForm-EditDisplayNameButton"));
+    await waitFor(() => {
+      expect(canvas.queryByTestId("UserForm-DisplayNameText")).not.toBeInTheDocument();
+      expect(canvas.queryByTestId("UserForm-EditDisplayNameButton")).not.toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-UpdateDisplayNameField")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DeleteAccountButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-CancelEditButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-SaveEditsButton")).toBeInTheDocument();
+    });
+
+    await userEvent.type(canvas.getByTestId("UserForm-UpdateDisplayNameField"), "Updated Name");
+    await userEvent.click(canvas.getByTestId("UserForm-SaveEditsButton"));
+    await waitFor(() => {
+      expect(canvas.queryByTestId("UserForm-UpdateDisplayNameField")).not.toBeInTheDocument();
+      expect(canvas.queryByTestId("UserForm-CancelEditButton")).not.toBeInTheDocument();
+      expect(canvas.queryByTestId("UserForm-SaveEditsButton")).not.toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DisplayNameText")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-EditDisplayNameButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DeleteAccountButton")).toBeInTheDocument();
+      expect(canvas.getByTestId("UserForm-DisplayNameText").textContent).toBe("Updated Name");
+    }, { timeout: 5000 });
+  }
+};
