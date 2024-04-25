2b3e92d333f07525b5e48e63f8dc794f257a45c1
JordanJamesTaylor <jordjamestaylor@gmail.com>
2024-04-25 18:29:59 -0400

    Added route to Community Outreach page

---
+++ src/pages/community-outreach/CommunityOutreachPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommunityOutreachPage = () => {
+
+    return (
+        <h1 style={{ marginTop: '10rem' }}>Community Outreach Page</h1>
+        
+    );
+};
+
+export default CommunityOutreachPage;
