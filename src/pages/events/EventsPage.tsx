9dcd75bbd5829091ac42d8a126d716f752cb4270
JordanJamesTaylor <jordjamestaylor@gmail.com>
2024-04-26 13:57:18 -0400

    Added buttons to change direction event cards are rendered to the Events page

--- src/pages/events/EventsPage.tsx
+++ src/pages/events/EventsPage.tsx
@@ -8,6 +8,9 @@ import Divider from '@mui/material/Divider';
 import GridViewIcon from '@mui/icons-material/GridView';
 import TableRowsIcon from '@mui/icons-material/TableRows';
 import EventCard from '../../shared/components/event-card/EventCard';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 
 type Event = {
    id: number,
@@ -21,15 +24,22 @@ type Event = {
 const EventsPage = () => {
     const [events, setEvents] = useState<any[]>([]);
     const { getEvents } = useContentful();
+    const [cardDirection, setCardDirection] = useState('column');
 
     useEffect(() => {
         getEvents().then((res: any) => setEvents(res.items));
     }, []);
 
+    const handleCardDirecion = (direction: string) => {
+        console.log(direction)
+        setCardDirection(direction);
+    };
 
     const displayEvents = events.map(event => {
         return (
-            <EventCard key={event.id} event={event} />
+            <ListItem sx={{display: 'flex', justifyContent: 'center', width: '100%' }}>
+                <EventCard key={event.id} event={event} />
+            </ListItem>
         );
     });
 
@@ -55,7 +65,7 @@ const EventsPage = () => {
                     href=""
                     sx={{
                         mr: 2,
-                        display: { xs: 'none', md: 'flex' },
+                        display: 'flex',
                         fontWeight: 700,
                         letterSpacing: '.15rem',
                         color: '#5E0916',
@@ -64,10 +74,14 @@ const EventsPage = () => {
                 >
                     Upcoming Events
                 </Typography>
-                <div>
-                    <GridViewIcon sx={{ color: '#5E0916' }} />
-                    <TableRowsIcon sx={{ marginLeft: '0.5rem', color: '#5E0916' }} />
-                </div>
+                <Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex' } }}>
+                    <Button variant="text" onClick={() => handleCardDirecion('row')} >
+                        <GridViewIcon sx={{ color: '#5E0916' }} />
+                    </Button>
+                    <Button variant="text" onClick={() => handleCardDirecion('column')} >
+                        <TableRowsIcon sx={{ marginLeft: '0.5rem', color: '#5E0916' }} />
+                    </Button>
+                </Box>
             </Stack>
             <Divider
                 sx={{
@@ -76,15 +90,9 @@ const EventsPage = () => {
                     backgroundColor: '#5E0916'
                 }}
             />
-            <Box
-                sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    flexDirection: 'column',
-                }}
-                >
+            <List sx={{ display: 'flex', flexDirection: cardDirection }} >
                 {displayEvents}
-            </Box>
+            </List>
         </Container>
     );
 };
