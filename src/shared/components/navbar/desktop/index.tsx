2ca8ccb4081ea6a142b49a9bbdf7be72223cbf3a
JordanJamesTaylor <jordjamestaylor@gmail.com>
2024-04-30 14:49:04 -0400

    Fixed dropdown orientation

--- src/shared/components/navbar/desktop/index.tsx
+++ src/shared/components/navbar/desktop/index.tsx
@@ -1,5 +1,5 @@
 import React, { SyntheticEvent, useState, useEffect } from 'react';
-import { Box, Container, AppBar, Toolbar, Typography, Tabs, Tab, InputLabel } from '@mui/material';
+import { Box, Container, AppBar, Toolbar, Typography, Tabs, Tab, InputLabel, MenuItem } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import axios from 'axios';
@@ -93,20 +93,24 @@ const DesktopNav: React.FC<DesktopNavProps> = ({ navItems }) => {
                                     labelId="demo-simple-select-filled-label"
                                     id="demo-simple-select-filled"
                                 >
-                                    <Tab 
-                                        key={'Our Mission'}
-                                        label={'Our Mission'}
-                                        component={Link}
-                                        to={`/Our Mission`}
-                                        data-testid={`DesktopNav-LinkTab-Our Mission`}
-                                    />
-                                    <Tab 
-                                        key={'Our Team'}
-                                        label={'Our Team'}
-                                        component={Link}
-                                        to={`/Our Team`}
-                                        data-testid={`DesktopNav-LinkTab-Our Team`}
-                                    />
+                                    <MenuItem>
+                                        <Tab 
+                                            key={'Our Mission'}
+                                            label={'Our Mission'}
+                                            component={Link}
+                                            to={`/Our Mission`}
+                                            data-testid={`DesktopNav-LinkTab-Our Mission`}
+                                        />
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <Tab 
+                                            key={'Our Team'}
+                                            label={'Our Team'}
+                                            component={Link}
+                                            to={`/Our Team`}
+                                            data-testid={`DesktopNav-LinkTab-Our Team`}
+                                        />
+                                    </MenuItem>
                                 </Select>
                             </FormControl>
                         </Tabs>
